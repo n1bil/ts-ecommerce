6c819ead1672106fde2011555a55d8393e8edc67
n1bil <nabil.nadzor@gmail.com>
2023-12-16 19:43:09 +0100

    List Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+    
 
-  return (
-    <div>
-      <header>
-        TS Amazona
-      </header>
-      <main>
-
-      </main>
-      <footer>
-        All right reserved
-      </footer>
-    </div>
-  )
+    return (
+        <div>
+            <header>TS Amazona</header>
+            <main>
+                <ul>
+                    {sampleProducts.map((product) => (
+                        <li key={product.slug}>
+                          <img src={product.image} alt={product.name} className="product-image" />
+                          <h2>{product.name}</h2>
+                          <p>${product.price}</p>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+            <footer>All right reserved</footer>
+        </div>
+    );
 }
 
-export default App
+export default App;
