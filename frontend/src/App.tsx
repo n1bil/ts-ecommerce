d698ed39fba7586cd142fac659681c0ec44c4749
n1bil <nabil.nadzor@gmail.com>
2023-12-16 20:50:06 +0100

    Add bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,44 @@
-import "./App.css";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import { sampleProducts } from "./data";
 
 function App() {
-    
-
     return (
-        <div>
-            <header>TS Amazona</header>
+        <div className="d-flex flex-column h-full">
+            <header>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Container>
+                        <Navbar.Brand>Amazon</Navbar.Brand>
+                    </Container>
+                    <Nav>
+                        <a href="/cart" className="nav-link">
+                            Cart
+                        </a>
+                        <a href="/signin" className="nav-link">
+                            Sign In
+                        </a>
+                    </Nav>
+                </Navbar>
+            </header>
             <main>
-                <ul>
-                    {sampleProducts.map((product) => (
-                        <li key={product.slug}>
-                          <img src={product.image} alt={product.name} className="product-image" />
-                          <h2>{product.name}</h2>
-                          <p>${product.price}</p>
-                        </li>
-                    ))}
-                </ul>
+                <Container className="mt-3">
+                    <Row>
+                        {sampleProducts.map((product) => (
+                            <Col key={product.slug} sm={6} md={4} lg={3}>
+                                <img
+                                    src={product.image}
+                                    alt={product.name}
+                                    className="product-image"
+                                />
+                                <h2>{product.name}</h2>
+                                <p>${product.price}</p>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
             </main>
-            <footer>All right reserved</footer>
+            <footer>
+                <div className="text-center">All right reserved</div>
+            </footer>
         </div>
     );
 }
