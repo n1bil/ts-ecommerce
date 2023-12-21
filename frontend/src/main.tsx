7487c98f7b0186cc081e04b9db2b59ae85c8e0a9
n1bil <nabil.nadzor@gmail.com>
2023-12-21 11:32:32 +0100

    Create React Context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.tsx";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
@@ -10,6 +10,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import ProductPage from "./pages/ProductPage.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Homepage from "./pages/Homepage.tsx";
+import { StoreProvider } from "./Store.tsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,17 +19,19 @@ const router = createBrowserRouter(
             <Route path="product/:slug" element={<ProductPage />} />
         </Route>
     )
-)
+);
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <HelmetProvider>
-            <QueryClientProvider client={queryClient}>
-                <RouterProvider router={router} />
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
-        </HelmetProvider>
+        <StoreProvider>
+            <HelmetProvider>
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={router} />
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
+            </HelmetProvider>
+        </StoreProvider>
     </React.StrictMode>
 );
