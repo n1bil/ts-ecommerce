44185265df8f8b2d7f648f30e48591be03a8230b
n1bil <nabil.nadzor@gmail.com>
2023-12-23 20:56:08 +0100

    Add to cart in Product page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from "react";
 import { Badge, Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { LinkContainer } from "react-router-bootstrap";
+import 'react-toastify/dist/ReactToastify.css';
 import { Store } from "./Store";
 
 function App() {
@@ -16,10 +19,13 @@ function App() {
 
     return (
         <div className="d-flex flex-column h-full">
+            <ToastContainer position="bottom-center" limit={1} />
             <header>
                 <Navbar expand="lg">
                     <Container>
-                        <Navbar.Brand>Amazon</Navbar.Brand>
+                        <LinkContainer to="/">
+                            <Navbar.Brand>Amazon</Navbar.Brand>
+                        </LinkContainer>
                     </Container>
                     <Nav>
                         <Button variant={mode} onClick={switchModeHandler}>
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,20 +1,39 @@
 import { Helmet } from "react-helmet-async";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetProductDetailsBySlugQuery } from "../hooks/productHooks";
 import { LoadingBox } from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-import { getError } from "../utils";
+import { convertProductToCartItem, getError } from "../utils";
 import { ApiError } from "../types/ApiError";
 import { Badge, Button, Card, Col, ListGroup, Row } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useContext } from "react";
+import { Store } from "../Store";
+import { toast } from "react-toastify";
 
 export default function ProductPage() {
     const { slug } = useParams();
-    const {
-        data: product,
-        isLoading,
-        error,
-    } = useGetProductDetailsBySlugQuery(slug!);
+    const { data: product, isLoading, error } = useGetProductDetailsBySlugQuery(slug!);
+
+    const {state, dispatch} = useContext(Store);
+    const { cart } = state;
+
+    const navigate = useNavigate();
+
+    const addToCartHandler = () => {
+        const existItem = cart.cartItems.find(cartItem => cartItem._id === product?._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        if (product!.countInStock < quantity) {
+            toast.warn('Sorry. Product is out of stock');
+            return;
+        }
+        dispatch({
+            type: 'CART_ADD_ITEM',
+            payload: { ...convertProductToCartItem(product!), quantity }, 
+        })
+        toast.success('Product added to the cart');
+        navigate('/cart');
+    }
 
     return isLoading ? (
         <LoadingBox />
@@ -83,7 +102,7 @@ export default function ProductPage() {
                                 {product.countInStock > 0 && (
                                   <ListGroup.Item>
                                     <div className="d-grid">
-                                      <Button variant="primary">Add to Cart</Button>
+                                      <Button onClick={() => addToCartHandler()} variant="primary">Add to Cart</Button>
                                     </div>
                                   </ListGroup.Item>
                                 )}
